c8499154c25bc0f3b533de42c6c931850d09448b
satakujp <dummy@sataku.jp>
2022-09-24 13:47:02 +0900

    Modify readme

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# scrapingpy-template
-You can test Web Scraping with Python and Docker
+# scrapingpy template
+
+BeautifulSoup4を使ってPythonでスクレイピングを実施するテンプレートプロジェクトです。
+次の処理を試しながらBeautifulSoupの基本を学習できます。
+
+1. Webページのタイトルを取得する。
+2. 要素をひとつ取得する。
+3. 複数の要素をを取得する。
+4. 要素の属性を取得する。
+5. 子要素を取得する。
+6. 【応用】JavaScriptが処理された後の要素を取得する。
+
+
+```フォルダ構成
+.
+├── README.md
+├── scraping            # projectファイル
+│       └── main.py    # discordpyスクリプト
+├── html                # スクレイピング対象のサンプルHTMLファイル一式
+├── .devcontainer       # Docker Remote Container用ファイル一式
+├── docker-compose      # Docker Compose用ファイル
+├── poetry.lock         # poetry依存関係
+└── pyproject.toml      # pythonプロジェクトにおける開発設定ファイル
+```
+
+## 使用言語
+* python
+
+## ライブラリ
+* beautifulsoup4
+* selene
+* requests
+* isort
+* black
+* flake8
+
+## 開発ツール
+* vscode
+* github codespace または remote conteiner(docker)
--- scraping/main.py
+++ scraping/main.py
@@ -1,7 +1,8 @@
 import requests
-import urllib.parse
+import os
 
 from bs4 import BeautifulSoup
+from selenium import webdriver
 
 # HTMLソースコードを取得してBeautifulSoupで解析する。　
 vgm_url = 'https://satakujp.z11.web.core.windows.net/'
@@ -37,3 +38,28 @@ print("\nn番目の子要素を取得する。")
 tr = soup.select("div > table > tr")
 print(tr[1].contents[3].get_text())
 print(soup.select_one("div > table > tr:nth-child(2) > td:nth-child(2)").get_text())
+
+# 番外編 JavaScriptが処理された後のスクレイピング
+# Seleniumと呼ばれるヘッドレスブラウザ（画面を持たないブラウザ）を利用します。
+    # Selenium サーバーへ接続する。
+options = webdriver.ChromeOptions()
+options.set_capability("loggingPrefs", {'performance': 'ALL'})
+
+# 画面を表示しないヘッドレスモードで動作する。
+options.add_argument('--headless')
+
+driver = webdriver.Remote(
+  command_executor=os.environ["SELENIUM_URL"],
+  options=options
+)
+# wait = WebDriverWait(driver, 10)
+
+driver.get("https://satakujp.z11.web.core.windows.net/")
+soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+print("\ncontentsで子要素をすべて取得できる。")
+tr = soup.select("div > table > tbody > tr")
+
+for ele in tr[2]:
+  if ele.name:
+    print("{0} : {1}".format(ele.name, ele.get_text()))
